a259e0312847dbe0e44420d5343ba8c15c602f8c
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2025-03-10 12:26:21 -0700

    running linters

--- scripts/data_transformation.py
+++ scripts/data_transformation.py
@@ -2,29 +2,31 @@
 Run data transformation locally
 """
 
+import logging
+import shutil
 import sys
-
-from aind_protein_data_transformation.models import ZeissJobSettings
-from aind_protein_data_transformation.zeiss_job import ZeissCompressionJob
-from typing import Optional
 from pathlib import Path
 from time import time
+from typing import Optional
+
+import bioio_czi
+import dask.array as da
+from bioio import BioImage
+
 from aind_protein_data_transformation.compress.czi_to_zarr import (
     czi_stack_zarr_writer,
 )
+from aind_protein_data_transformation.models import ZeissJobSettings
 from aind_protein_data_transformation.utils import utils
-from bioio import BioImage
-import bioio_czi
-import dask.array as da
-import logging
-import shutil
+from aind_protein_data_transformation.zeiss_job import ZeissCompressionJob
+
 
 def transform_pan_protein_data(
     input_source: str,
     output_dir: str,
     s3_location: str,
     num_partitions: Optional[int] = 1,
-    remove_stacks: Optional[bool] = False
+    remove_stacks: Optional[bool] = False,
 ):
     """
     Transforms pan protein data.
@@ -36,7 +38,7 @@ def transform_pan_protein_data(
 
     output_dir: str
         Path where we want to write the zarrs.
-    
+
     num_partitions: Optional[int]
         Number of partitions. Default: 1
     """
@@ -48,26 +50,28 @@ def transform_pan_protein_data(
         s3_location=s3_location,
     )
     basic_job = ZeissCompressionJob(job_settings=basic_job_settings)
-    
+
     # Commenting it out since there are datasets which could have
     # multiple channels, I prefer to have each zarr as a channel
     # basic_job.run_job()
-    
+
     compressor = basic_job._get_compressor()
-    
+
     job_start_time = time()
 
     partitioned_list = basic_job._get_partitioned_list_of_stack_paths()
     stacks_to_process = partitioned_list[
         basic_job.job_settings.partition_to_process
     ]
-    
+
     for stack in stacks_to_process:
         print(f"Converting {stack}")
         stack_name = stack.stem
         root_name = stack.parent.stem
 
-        output_path = Path(basic_job.job_settings.output_directory).joinpath(stack_name)
+        output_path = Path(basic_job.job_settings.output_directory).joinpath(
+            stack_name
+        )
 
         czi_file_reader = BioImage(str(stack), reader=bioio_czi.Reader)
 
@@ -104,9 +108,7 @@ def transform_pan_protein_data(
 
             if basic_job.job_settings.s3_location is not None:
                 channel_zgroup_file = output_path / ".zgroup"
-                s3_channel_zgroup_file = (
-                    f"{basic_job.job_settings.s3_location}/{stack_name}/.zgroup"
-                )
+                s3_channel_zgroup_file = f"{basic_job.job_settings.s3_location}/{stack_name}/.zgroup"
                 print(
                     f"Uploading {channel_zgroup_file} to "
                     f"{s3_channel_zgroup_file}"
@@ -115,14 +117,14 @@ def transform_pan_protein_data(
                     channel_zgroup_file, s3_channel_zgroup_file
                 )
                 ome_zarr_curr_stack_name = f"{curr_stack_name}.ome.zarr"
-                ome_zarr_stack_path = output_path.joinpath(ome_zarr_curr_stack_name)
+                ome_zarr_stack_path = output_path.joinpath(
+                    ome_zarr_curr_stack_name
+                )
                 s3_stack_dir = (
                     f"{basic_job.job_settings.s3_location}/{stack_name}/"
                     f"{ome_zarr_curr_stack_name}"
                 )
-                print(
-                    f"Uploading {ome_zarr_stack_path} to {s3_stack_dir}"
-                )
+                print(f"Uploading {ome_zarr_stack_path} to {s3_stack_dir}")
                 utils.sync_dir_to_s3(ome_zarr_stack_path, s3_stack_dir)
                 print(f"Removing: {ome_zarr_stack_path}")
                 # Remove stack if uploaded to s3. We can potentially do all
@@ -134,8 +136,9 @@ def transform_pan_protein_data(
     total_job_duration = time() - job_start_time
     print(f"Total job duration: {total_job_duration}")
 
+
 if __name__ == "__main__":
-    BASE_PATH = Path("/allen/aind/scratch/camilo.laiton/pan_protein_data_deep_learning")
+    BASE_PATH = Path("/path/to/your/dataset")
     input_source = BASE_PATH.joinpath("CZI_data")
     output_dir = BASE_PATH.joinpath("OMEZarr_data")
 
@@ -145,5 +148,5 @@ if __name__ == "__main__":
             input_source=str(input_source),
             output_dir=str(output_dir),
             num_partitions=1,
-            s3_location="s3://aind-msma-morphology-data/test_data/protein_project"
+            s3_location="s3://some-bucket/test_data/protein_project",
         )
