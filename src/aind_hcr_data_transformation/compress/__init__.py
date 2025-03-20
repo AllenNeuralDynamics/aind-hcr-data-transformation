2e53c7c390af7af16d9ab73a2a14f7815a8b937d
Camilo Laiton <36769694+camilolaiton@users.noreply.github.com>
2025-03-20 14:52:59 -0700

    updating inits

--- src/aind_hcr_data_transformation/compress/__init__.py
+++ src/aind_hcr_data_transformation/compress/__init__.py
@@ -1,19 +1,3 @@
 """
-CZI to Zarr stack conversion
+CZI compression
 """
-
-__version__ = "0.0.1"
-__authors__ = ["Camilo Laiton"]
-__author_emails__ = [
-    "camilo.laiton@alleninstitute.org",
-]
-__license__ = "MIT"
-__description__ = "Repository for transforming CZI data to ome.zarr"
-__url__ = "https://github.com/AllenNeuralDynamics/aind-hcr-data-transformation"
-
-__maintainers__ = ["Camilo Laiton"]
-__maintainer_emails__ = [
-    "camilo.laiton@alleninstitute.org",
-]
-__title__ = "aind-protein-hcr-transformation"
-__status__ = "Production"  # 'Development' 'Production', 'Beta'
